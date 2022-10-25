98e2023477cb8b07b74b67434d8ac912f60c167a
cyber-samurai33 <cyber-samurai@tutanota.com>
2022-10-25 16:00:15 -0300

    commit

--- madscript_highschool_1.py
+++ madscript_highschool_1.py
@@ -6,20 +6,18 @@ class Highschool:
     verb = str(input("Verb: "))
     adjective = str(input("Adjective: "))
     adverb = str(input("Adverb: "))
-
-    print(f""" 
-    During my sophomore year of high school, 
-    we were doing silent work and my history {adjective} 
-    said that we could listen to music but if it was too 
-    loud he would “{verb} our {noun}.” 
-    so I’m doing my work quietly with my music on low,
-     and this obnoxious kid sitting next to me had his 
-     music really {verb}. I could {verb} it over my music 
-     but ignored it. My teacher thought it was me. 
-     So he comes up to me & {adjective} my BRAND NEW Apple headphones,
-      looking ruthless. He suddenly realized it was the guy 
-      next to me and he was completely {adverb}. He came in 
-      the next day with a new pair and an apology note taped to them. 
-      He couldn’t look me in the eye for the rest of the year.
-
-    """ )
+    print('\t\t\t\t\t High School\n')
+    print(f'''During my sophomore year of high school,\n 
+    \twe were doing silent work and my history {adjective}\n 
+    \tsaid that we could listen to music but if it was too\n 
+    \tloud he would “{verb} our {noun}.”\n 
+    \tso I’m doing my work quietly with my music on low,\n
+    \tand this obnoxious kid sitting next to me had his\n 
+    \tmusic really {verb}. I could {verb} it over my music\n 
+    \tbut ignored it. My teacher thought it was me.\n 
+    \tSo he comes up to me & {adjective} my BRAND NEW Apple headphones,\n
+    \tlooking ruthless. He suddenly realized it was the guy\n 
+    \tnext to me and he was completely {adverb}. He came in\n 
+    \tthe next day with a new pair and an apology note taped to them.\n 
+    \tHe couldn’t look me in the eye for the rest of the year.\n
+    \t''' )
