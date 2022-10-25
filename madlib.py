9b4c1859704d18f052835ebf32b474221c41af45
cyber-samurai33 <cyber-samurai@tutanota.com>
2022-10-25 15:38:26 -0300

    commit

--- madlib.py
+++ madlib.py
@@ -1,32 +1,26 @@
 import sys
 import os
 import time
-# When user makes wrong selection, add a function to return to title - 
-# screen.
-
+from keyboard import press
+# Create a while loop to force user to select or go back to title.
 
 # Welcome to Mad Lib! Banner
 print('Welcome to Mad Libs! Select your story!')
-time.sleep(2)
 print('')
 print(''' [1] Sam
- [2] High School''')
+ [2] High School
+  ''')
 
 # Mad lib Title Selection
 
-title = int(input())
+title = int(input("> "))
 
-sam = 1
-highschool = 2
-
-# [1] Sam Selection / [2] High School Selection
-if title == 1:
-   import madscript_sam    
+if title == 1: 
+   import madscript_sam
 elif title == 2:
    import madscript_highschool_1
-else:
-   print("Retry Selection..")
-   
+else: 
+   import madlib
 
 
 
