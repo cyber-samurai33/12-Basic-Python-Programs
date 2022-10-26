c529b3bf2013af7c70c95fb1be1fb1335b598ba5
cyber-samurai33 <cyber-samurai@tutanota.com>
2022-10-26 17:48:34 -0300

    commit

--- Guess the number(computer)/Guess the number(computer).py
+++ Guess the number(computer)/Guess the number(computer).py
@@ -9,4 +9,8 @@ def guess(x):
     guess = 0 
     while guess != random_number:
         guess = input(f'Guess a number between 1 and {x}: ')
+        if guess < random.number:
+            print('Sorry, guess again, Too Low. ')
+        elif guess > random_number:
+            print('Sorry, guess again. Too High. ')
 guess(10)
