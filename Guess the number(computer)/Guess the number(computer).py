3f7536b243840dc70c93800b62f4c89166e4215b
cyber-samurai33 <cyber-samurai@tutanota.com>
2022-10-26 18:13:57 -0300

    commit

--- Guess the number(computer)/Guess the number(computer).py
+++ Guess the number(computer)/Guess the number(computer).py
@@ -8,9 +8,30 @@ def guess(x):
     random_number = random.randint(1,x)
     guess = 0 
     while guess != random_number:
-        guess = input(f'Guess a number between 1 and {x}: ')
+        guess = int(input(f'Guess a number between 1 and {x}: '))
         if guess < random_number:
             print('Sorry, guess again, Too Low. ')
         elif guess > random_number:
             print('Sorry, guess again. Too High. ')
-guess(10)
+        elif guess == random_number:
+            print("You guessed it!")
+
+
+def computer_guess(x):
+    low = 1
+    high =x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low  # Could also be high
+        feedback = input(f'is {guess} too high (H), too low (L), or correct (C)??').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low == guess + 1
+    print(f'Yay! The computer guessed your number!, {guess}, correctly!')
+
+
+computer_guess(10)
