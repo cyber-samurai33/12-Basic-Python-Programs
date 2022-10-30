33ab99b44078ec9120842bfbd6bd6d7689b2a574
cyber-samurai33 <cyber-samurai@tutanota.com>
2022-10-29 22:38:42 -0300

    commit

--- madlib/madscript_highschool.py
+++ madlib/madscript_highschool.py
@@ -1,18 +1,18 @@
 import os
 
-class HighSchool:
+
     
-    noun = str(input("Noun: "))
-    verb = str(input("Verb: "))
-    adjective = str(input("Adjective: "))
-    adverb = str(input("Adverb: "))
-    os.system("clear||cls")
-    print('\t\t\t\t\t  High School\n')
-    print(f'''\tDuring my sophomore year of high school,we were doing silent work and my history {adjective}\n 
-    \t said that we could listen to music but if it was too loud he would “{verb} our {noun}.”\n 
-    \t so I’m doing my work quietly with my music on low,and this obnoxious kid sitting next to me had his\n 
-    \t music really {verb}. I could {verb} it over my music but ignored it. My teacher thought it was me.\n 
-    \t So he comes up to me & {adjective} my BRAND NEW Apple headphones,looking ruthless. He suddenly realized\n 
-    \t it was the guy next to me and he was completely {adverb}. He came in the next day with a new pair and an\n 
-    \t apology note taped to them.He couldn’t look me in the eye for the rest of the year.\n
-    \t ''' )
+noun = str(input("Noun: "))
+verb = str(input("Verb: "))
+adjective = str(input("Adjective: "))
+adverb = str(input("Adverb: "))
+os.system("clear||cls")
+print('\t\t\t\t\t  High School\n')
+print(f'''\tDuring my sophomore year of high school,we were doing silent work and my history {adjective}\n 
+\t said that we could listen to music but if it was too loud he would “{verb} our {noun}.”\n 
+\t so I’m doing my work quietly with my music on low,and this obnoxious kid sitting next to me had his\n 
+\t music really {verb}. I could {verb} it over my music but ignored it. My teacher thought it was me.\n 
+\t So he comes up to me & {adjective} my BRAND NEW Apple headphones,looking ruthless. He suddenly realized\n 
+\t it was the guy next to me and he was completely {adverb}. He came in the next day with a new pair and an\n 
+\t apology note taped to them.He couldn’t look me in the eye for the rest of the year.\n
+\t ''' )
--- madlib/madscript_sam.py
+++ madlib/madscript_sam.py
@@ -1,18 +1,18 @@
 import os
 
 
-class Sam:
+
     
-    noun = str(input("Noun: "))
-    verb = str(input("Verb: "))
-    adjective = str(input("Adjective: "))
-    adverb = str(input("Adverb: "))
-    os.system("clear||cls")
+noun = str(input("Noun: "))
+verb = str(input("Verb: "))
+adjective = str(input("Adjective: "))
+adverb = str(input("Adverb: "))
+os.system("clear||cls")
        
-    print('\t\t\t\t\t Sam The Monkey \n')
-    print(f'''\tThis is a story about a monkey name Sam. Sam was curious about many things,\n 
-    \tsuch as {noun} Sam would {verb} all day and night when there were loud noise's.\n
-    \tSam would often think of {adjective} when he was sitting on a branch.\n 
-    \tSometimes Sam would have to {adverb} a tree to avoid getting eaten by other predators.\n
-    \tSam was mostly interested in bananas and throwing poop, this was a daily activity for Sam.\n 
-    ''' )
+print('\t\t\t\t\t Sam The Monkey \n')
+print(f'''\tThis is a story about a monkey name Sam. Sam was curious about many things,\n 
+\tsuch as {noun} Sam would {verb} all day and night when there were loud noise's.\n
+\tSam would often think of {adjective} when he was sitting on a branch.\n 
+\tSometimes Sam would have to {adverb} a tree to avoid getting eaten by other predators.\n
+\tSam was mostly interested in bananas and throwing poop, this was a daily activity for Sam.\n 
+''' )
